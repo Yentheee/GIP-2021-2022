5a0893fe75eb520326c170bc626bff970490d72e
Yenthe <yenthe6@live.be>
2022-05-10 17:05:36 +0200

    update

--- GIP Smart/Pages/Classes/connStrings.cs
+++ GIP Smart/Pages/Classes/connStrings.cs
@@ -8,11 +8,11 @@ namespace GIP_Smart.Pages
     public class connStrings
     {
         //school PC
-        //static public string connString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\YentheVandenEynden\OneDrive - GITOK Bovenbouw\GIP Yenthe-Nicolas\Software\DatabaseGIP.accdb;Persist Security Info=False;";
+        static public string connString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\YentheVandenEynden\OneDrive - GITOK Bovenbouw\GIP Yenthe-Nicolas\Software\DatabaseGIP.accdb;Persist Security Info=False;";
         
 
         //Thuis PC
-        static public string connString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\yenth\OneDrive - GITOK Bovenbouw\GIP Yenthe-Nicolas\Software\DatabaseGIP.accdb;Persist Security Info=False;";
+        //static public string connString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\yenth\OneDrive - GITOK Bovenbouw\GIP Yenthe-Nicolas\Software\DatabaseGIP.accdb;Persist Security Info=False;";
 
     }
     
